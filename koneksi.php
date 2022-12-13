7afdda2ad4aba1c98e321c94dc580461e42e0a5e
Pandunanda P. Kafah <pandukyouka@gmail.com>
2022-12-13 14:32:14 +0700

    aa
    aaa

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php 
+$host="localhost";
+$user="root";
+$pass="";
+$name="ppdj1579_dbdaftaronline";
+
+$koneksi= new mysqli($host,$user,$pass,$name);
+// or die("Koneksi database gagal . :(")
+// mysqli_select_db($name,$koneksi) or die("Tidak ada database terpilih :(");
+?>
